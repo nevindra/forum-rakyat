f2328675409f7e288a83f4ec5b8a2e01bb79de39
r17x <ri7nz@evilfactory.id>
2024-08-27 10:32:36 +0700

    chore(ui): add new components shadcn-ui
    command:
    `$ pnpx shadcn-ui@latest add input`

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+export * from './separator'
+export * from './accordion'
+export * from './button'
+export * from './card'
+export * from './form'
+export * from './input'
+export * from './label'
+
